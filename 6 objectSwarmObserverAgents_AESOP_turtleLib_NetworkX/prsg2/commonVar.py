48301a5f3f32d862ded68c4d1aaeb8019b2291e3
javatechs <javatechs@gmail.com>
2017-08-03 15:08:05 -0700

    Initial commit
    _ Visualization
    _ Start customizing for robots

---
+++ 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/prsg2/commonVar.py
@@ -0,0 +1,13 @@
+#commonVar.py
+
+projectVersion = 1.0
+
+# Executed at the end of behavior
+toBeExecuted="print 'Sequence terminated.'"
+
+# TODO: This could be a ROS observer action. See oActions.py
+# TODO: Should this be renamed?
+specialAction="makeSpecialAction()"
+
+# TODO From debug project. Do we need this?
+#debug = False  # to learn, start with False and then try True
